b5c8e0105134d462b07ef97802cce469ad3f7e0b
Brian Manlove <47579080+N129BZ@users.noreply.github.com>
2021-09-02 18:16:45 -0500

    Update dgramtest.py

--- python/dgramtest.py
+++ python/dgramtest.py
@@ -1,1 +1,16 @@
+import socket
+from kaitaistruct import KaitaiStream, BytesIO
+from ahrs_record import AhrsRecord
+import json
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
+udp_host = "0.0.0.0" 
+udp_port = 4000	
+	                
+sock.bind((udp_host,udp_port))
+
+while True:
+    data,addr = sock.recvfrom(128)
+    if data[1] == 76:
+        print(AhrsRecord(KaitaiStream(BytesIO(data))))
+    
