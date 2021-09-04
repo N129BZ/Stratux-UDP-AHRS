e863f2561c176e3c2672f456d8302bf1cbdff43f
Brian Manlove <47579080+N129BZ@users.noreply.github.com>
2021-09-03 21:26:31 -0500

    Add files via upload

---
+++ UDPReader/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json;
+using Kaitai;
+
+namespace UDPReader
+{
+    class Program
+    {
+        const int PORT = 4000;
+        static Thread doWork;
+
+        static void Main(string[] args)
+        {   
+            doWork = new Thread(new ThreadStart(startReading));
+
+            doWork.Start();
+
+            Console.Read();
+        }
+
+        static void OnReaderMessage(Byte[] bytes) {
+            AhrsRecord ahrs = new AhrsRecord(new KaitaiStream(bytes));
+            Console.WriteLine(ahrs.Record.ToString());        
+        }
+
+        static void startReading() {
+            DgramReader reader = new DgramReader();
+            reader.OnMessageReceived += OnReaderMessage;
+            reader.Run(PORT);
+        }
+    }
+
+    class DgramReader 
+    {
+        public delegate void OnMessageReceivedDelegate(Byte[] message);
+        public OnMessageReceivedDelegate OnMessageReceived;
+
+        public void Run(int port) {
+            
+            UdpClient receivingUdpClient = new UdpClient(port);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            
+            try {
+                while (true) {
+                    Byte[] buffer = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                    if (buffer[1] == 76) {
+                        OnMessageReceived?.Invoke(buffer);
+                    }
+                }
+            }
+            catch ( Exception e ) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
